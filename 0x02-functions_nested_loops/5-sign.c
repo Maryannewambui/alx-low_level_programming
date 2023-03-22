bad18a99de1f0a846b214009a7f0d9826da7f099
Maryannewambui <trimbleleewambui@gmail.com>
2023-03-21 18:10:10 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *print_sign - prints sighn of numbers
+ *@n: parameter check
+ *
+ *Return: 1, and 0 and -1
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
