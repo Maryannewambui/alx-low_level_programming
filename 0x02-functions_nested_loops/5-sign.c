deb136249b39ce8b20536456e03da0c0dc9c97f9
Maryannewambui <trimbleleewambui@gmail.com>
2023-03-21 21:43:59 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *print_sign - prints sign of numbers
  *@n: parameter check
@@ -10,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0, ");
+		_putchar(48);
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
-		printf("-, ");
+		_putchar('-');
 		return (-1);
 	}
 }
