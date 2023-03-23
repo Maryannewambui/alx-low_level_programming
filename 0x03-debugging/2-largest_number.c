e7ce1cf5dd33725421dfdedc55d05668bdad9ce7
Maryannewambui <trimbleleewambui@gmail.com>
2023-03-23 15:15:25 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -23,9 +23,13 @@ int largest_number(int a, int b, int c)
 		largest = b;
 	}
 	else if (b > c && c > a)
+	{
 		largest = b;
+	}
 	else if (c > a && a > b)
+	{
 		largest = c;
+	}
 	else
 	{
 		largest = c;
