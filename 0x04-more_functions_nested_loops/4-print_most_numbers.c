8e7a734a8f6185e6f2c66128b3f19cffd09b5fbf
Maryannewambui <trimbleleewambui@gmail.com>
2023-03-24 16:17:46 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
+ * print_most_numbers - prints number 2 and 4
  *
- *
- *
+ *return: void
  */
 void print_most_numbers(void)
 {
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2'|| c == '4'))
+		if (!(c == '2' || c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
