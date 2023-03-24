caadbbc588a82761ad51b920b44b1fb55dd485ef
Maryannewambui <trimbleleewambui@gmail.com>
2023-03-24 15:49:42 -0700

    collaboration and multiplication of integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * mul - 
+ * mul - multiply 2 integers
  * @a: first integer
  * @b: second integer
  * Return: addition of multiplication
  */
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
