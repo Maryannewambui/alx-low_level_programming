058295cf46e02824352fcd59d90662f6cfe87f8d
Maryannewambui <trimbleleewambui@gmail.com>
2023-04-18 19:19:51 -0700

    C - preprocessor task

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+ * main - function that check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	a = 98 + SIZE;
+	printf("%d\n", a);
+	return (0);
+}
