769921fd685532ae9282e04cfd201e5b6850ece6
Maryannewambui <trimbleleewambui@gmail.com>
2023-04-21 00:09:51 -0700

    3. a function that prints anything.

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * main - function that checks the code
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	print_all("ceis", 'B', 3, "stSchool");
+	return (0);
+}
