11ba7410ea1cdf58f95635f67e6ba394143aa118
Maryannewambui <trimbleleewambui@gmail.com>
2023-04-20 23:57:52 -0700

    2. a function that prints strings, followed by a new line

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * main - function that checks the code
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
